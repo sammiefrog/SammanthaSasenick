f958cce484cff484adee2cd593e1b95141827bbf
sammiefrog <sammiefrog@live.com>
2020-06-30 15:09:55 -0500

    Updates

---
+++ precache-manifest.e709d09f3eb8b5152f4b6c37df7ae5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c60d4e4540d8987d7f98a42280a0c92",
+    "url": "/SammanthaSasenick/index.html"
+  },
+  {
+    "revision": "3cb52e75051a43db1dfa",
+    "url": "/SammanthaSasenick/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "cc091b22bf05c962b080",
+    "url": "/SammanthaSasenick/static/js/2.dd0cd057.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SammanthaSasenick/static/js/2.dd0cd057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb52e75051a43db1dfa",
+    "url": "/SammanthaSasenick/static/js/main.dabda9fa.chunk.js"
+  },
+  {
+    "revision": "cb77c48c2df56f6ac1fb",
+    "url": "/SammanthaSasenick/static/js/runtime-main.576b7dfe.js"
+  }
+]);
